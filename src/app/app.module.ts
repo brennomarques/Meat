146850ce0ab7ada8e1856a2bcc2e1ae15a5da2be
brenno.lima <brenno.lima@soluti.com.br>
2020-11-13 11:46:16 -0300

    implementado carinho

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,8 +9,14 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component'
-import { from } from 'rxjs/observable/from';
 import { RestaurantsComponent } from './restaurants/restaurants.component';
+import { RestaurantComponent } from './restaurants/restaurant/restaurant.component';
+import { RestaurantsService } from './restaurants/restaurants.service';
+import { RestaurantDetailComponent } from './restaurant-detail/restaurant-detail.component';
+import { MenuComponent } from './restaurant-detail/menu/menu.component';
+import { ShoppingCardComponent } from './restaurant-detail/shopping-card/shopping-card.component';
+import { MenuItemComponent } from './restaurant-detail/menu-item/menu-item.component'
+
 
 
 @NgModule({
@@ -19,14 +25,19 @@ import { RestaurantsComponent } from './restaurants/restaurants.component';
     HeaderComponent,
     HomeComponent,
     AboutComponent,
-    RestaurantsComponent
+    RestaurantsComponent,
+    RestaurantComponent,
+    RestaurantDetailComponent,
+    MenuComponent,
+    ShoppingCardComponent,
+    MenuItemComponent,
   ],
   imports: [
     BrowserModule,
     HttpModule,
     RouterModule.forRoot(ROUTES)
   ],
-  providers: [],
+  providers: [RestaurantsService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,7 @@ import {Routes} from '@angular/router'
 import {HomeComponent} from './home/home.component'
 import {AboutComponent} from './about/about.component'
 import {RestaurantsComponent} from './restaurants/restaurants.component'
+import {RestaurantDetailComponent} from './restaurant-detail/restaurant-detail.component'
 
 
 export const ROUTES: Routes = [
@@ -11,5 +12,6 @@ export const ROUTES: Routes = [
   {path: '', component: HomeComponent},
   {path: 'about', component: AboutComponent},
   {path: 'restaurants', component: RestaurantsComponent},
+  {path: 'restaurants/:id', component: RestaurantDetailComponent}
 
 ]
